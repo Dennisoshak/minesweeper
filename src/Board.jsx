ea9abd7d0338e534a3f9c77fe9b391c4fb475723
dennisoshak <dennisoshak@gmail.com>
2023-07-04 15:09:27 +0300

    Game won

--- src/Board.jsx
+++ src/Board.jsx
@@ -4,9 +4,9 @@ import Cell from "./Cell";
 
 const Board = () => {
   const [cells, setCells] = useState([]);
-  const [status,setStatus] = useState('')
-  const [flagCount,setFlagCount] = useState(10)
-const [count,setCount]=useState(0)
+  const [status, setStatus] = useState("");
+  const [flagCount, setFlagCount] = useState(10);
+  const [count, setCount] = useState(0);
   const fillCells = () => {
     const tempCells = [];
     for (let i = 0; i < 9; i++) {
@@ -30,26 +30,45 @@ const [count,setCount]=useState(0)
     setCells(newCells);
   };
 
-
   useEffect(() => {
     fillCells();
     // eslint-disable-next-line
   }, []);
-  useEffect(()=>{
-count===46 && setStatus('You WIn')
-  },[count])
+  useEffect(() => {
+    count === 46 && setStatus("Well done!");
+    
+  }, [count]);
 
   return (
     <div className="board">
       <h1 style={{ textAlign: "center" }}>Minesweeper</h1>
-      <div style={{ textAlign: "center",height:"2rem"}}><h3>{status}</h3></div>
-      <h3 style={{marginLeft:'2rem'}}>Flags: {flagCount}</h3>
-     {status &&<h3 onClick={()=>{window.location.reload()}}style={{ textAlign: "center",cursor:"pointer" }}>{"Restart"}</h3>}
-      
+      <div style={{ textAlign: "center", height: "2rem" }}>
+        <h3>{status}</h3>
+      </div>
+      <h3 style={{ marginLeft: "2rem" }}>
+        Flags: {flagCount }<span style={{marginLeft:"1rem"}}> Bombs:10</span>
+      </h3>
+      {status && (
+        <h3
+          onClick={() => {
+            window.location.reload();
+          }}
+          style={{ textAlign: "center", cursor: "pointer" }}
+        >
+          {"Restart"}
+        </h3>
+      )}
+
       <div className="board-container">
-        {cells.map((cell,i) => (
+        {cells.map((cell, i) => (
           <div key={i} className="cell">
-         <Cell cell={cell} setStatus={setStatus} status={status} setFlagCount={setFlagCount} setCount={setCount}/>
+            <Cell
+              cell={cell}
+              setStatus={setStatus}
+              status={status}
+              setFlagCount={setFlagCount}
+              setCount={setCount}
+            />
           </div>
         ))}
       </div>
--- src/Cell.jsx
+++ src/Cell.jsx
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
-const bomb = "💣";
+import React, { useEffect, useState } from "react";
 const flag = "🚩";
 
-const Cell = ({ cell, setStatus, status, setFlagCount,setCount }) => {
+const Cell = ({ cell, setStatus, status, setFlagCount, setCount }) => {
   const [value, setValue] = useState("");
+  const bomb = status === "Well done!" ? "💣" : "💥";
+  console.log(status);
   const handleClick = (e) => {
     if (status === "Game over") return;
     setValue(cell.isBomb ? bomb : cell.neighbour);
-    setCount((prev)=>prev+1)
+    setCount((prev) => prev + 1);
     cell.isBomb && setStatus("Game over");
   };
   const handleFlag = (e) => {
@@ -20,6 +21,12 @@ const Cell = ({ cell, setStatus, status, setFlagCount,setCount }) => {
       setFlagCount((prev) => prev + 1);
     }
   };
+
+  useEffect(() => {
+    status === "Well done!" && setValue(cell.isBomb ? bomb : cell.neighbour);
+    // eslint-disable-next-line
+  }, [status]);
+
   return (
     <div
       style={status === "Game over" ? { pointerEvents: "none" } : {}}
